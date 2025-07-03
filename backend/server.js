842fd94c29b246999505a084fe33bf6e9484c97f
Deepak <jangiddee999@gmail.com>
2025-07-03 20:38:44 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 import express from "express";
 
 const app = express();
+app.get("/api/notes",(req,res)=>{
+    //delete a note
+    res.send("you got 5 notes");
+})
 
 app.listen(5001,() => {
     console.log("server started on port: 5001");
