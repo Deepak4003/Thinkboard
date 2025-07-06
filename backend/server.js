9f0e005adc778be0aba5088e12aae81c9c186918
Deepak <jangiddee999@gmail.com>
2025-07-06 17:54:43 +0530

    optimize file and folder

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,9 @@
 import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
 
 const app = express();
+app.use("/api/notes" , notesRoutes);
 
-
-app.get("/api/notes",(req,res)=>{
-    res.status(200).send("you got  10 notes");
-});
-
-app.post("/api/notes",(req,res)=>{
-    res.status(201).json({message:"note created successfully"});
-});
-
-app.put("/api/notes/:id",(req,res)=>{
-    res.status(200).json({message:"note updated successfully"});
-});
-
-app.delete("/api/notes/:id",(req,res)=>{
-    res.status(200).json({message:"note deleted successfully"});
-});
 
 
 app.listen(5001,() => {
