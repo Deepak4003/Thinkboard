a6e2f7562e7c2584fbf61003dafa7ba019455e91
Deepak <jangiddee999@gmail.com>
2025-07-06 22:12:46 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 
 const app = express();
-app.use("/api/notes" , notesRoutes);u
+app.use("/api/notes" , notesRoutes);
 
 
 
