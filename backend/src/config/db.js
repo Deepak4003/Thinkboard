670333993d16dc28ff5b519c5ee588a3b2ca9a80
Deepak <jangiddee999@gmail.com>
2025-07-08 22:58:40 +0530

    setup mongoose

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const connectDB = async () => {
     try {
-       await mongoose.connect("mongodb+srv://jangiddee999:0zL2jAy16eEKSyIR@cluster0.5lyriby.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-       console.log("MongoDB connected successfully");
-        
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("MongoDB connected successfully");
     } catch (error) {
-        console.error("Error connecting to MongoDB", error);
-        
+        console.error("Error connecting to MongoDB:", error.message);
     }
 };
