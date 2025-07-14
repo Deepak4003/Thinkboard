38b315d2076a6de87e0ac05b9395f7541fa72341
Deepak <jangiddee999@gmail.com>
2025-07-14 12:24:52 +0530

    finalize backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import {connectDB} from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config();
 
 console.log(process.env.MONGO_URI);
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 5001;
 connectDB();
 
 app.use(express.json()); // middleware
+app.use(rateLimiter);
 
 app.use((req, res, next) => {
     console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
@@ -20,7 +22,9 @@ app.use((req, res, next) => {
 
 app.use("/api/notes" , notesRoutes);
 
-app.listen(PORT,() => {
-    console.log("server started on port:", PORT);
-});
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
+  });
 
